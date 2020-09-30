ae724cb1b39e1fda2b16a08400b547399783674f
ethan sayagh <ethan.sayagh@gmail.com>
2020-09-30 19:24:10 +0300

    first commit

--- src/pages/loading/index.js
+++ src/pages/loading/index.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import '../loading/style.css';
-import { Redirect } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import {Helmet} from 'react-helmet';
+import React, { Component } from "react";
+import "../loading/style.css";
+import { Redirect } from "react-router-dom";
+import ReactGA from "react-ga";
+import { Helmet } from "react-helmet";
 import PuffLoader from "react-spinners/PuffLoader";
 
 class Loading extends Component {
-
   state = {
-    redirect: false
-  }
+    redirect: false,
+  };
 
   componentDidMount() {
-     this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
-    //--Motasim Foads Portfolio website Google analytics--
+    this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
+    //--Ethan Sayagh Portfolio website Google analytics--
     //--Replace with your own key !!
     ReactGA.initialize("UA-154721739-1");
-    ReactGA.pageview('motasimfoad.com - Loading Screen');
-    //--Motasim Foads Portfolio website Google analytics--
+    ReactGA.pageview("motasimfoad.com - Loading Screen");
+    //--Ethan Sayagh Portfolio website Google analytics--
   }
-    
+
   componentWillUnmount() {
-    clearTimeout(this.id)
+    clearTimeout(this.id);
   }
 
   render() {
-    return this.state.redirect
-      ? <Redirect to="/home" />
-      : <div>
-         <Helmet>
-                <meta charSet="utf-8" />
-                <title>Motasim Foad</title>
-                <link rel="canonical" href="http://motasimfoad.com/" />
-                <meta name="description" content="Motasim Foad - Product Manager | Project Manager | Software Engineer" />
-         </Helmet>
-        <div className="Loading-header">
-          <PuffLoader
-            size={'40vw'}
-            color={"#fff"}
-            loading={true}
+    return this.state.redirect ? (
+      <Redirect to="/home" />
+    ) : (
+      <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Ethan Sayagh</title>
+          <link rel="canonical" href="http://motasimfoad.com/" />
+          <meta
+            name="description"
+            content="Ethan Sayagh - Product Manager | Project Manager | Software Engineer"
           />
+        </Helmet>
+        <div className="Loading-header">
+          <PuffLoader size={"40vw"} color={"#fff"} loading={true} />
         </div>
-        </div>
+      </div>
+    );
   }
 }
 
