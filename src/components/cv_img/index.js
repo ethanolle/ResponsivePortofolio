70d3d5bfe6fe85e6f22a83fbf8002ed250d629a0
ethan sayagh <ethan.sayagh@gmail.com>
2020-10-08 13:24:48 +0300

    hiden over-x updated

--- src/components/cv_img/index.js
+++ src/components/cv_img/index.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import '../cv_img/style.css';
-import cv1 from '../../assets/img/cv1.png';
-import cv2 from '../../assets/img/cv2.png';
-import cv3 from '../../assets/img/cv3.png';
-import cv4 from '../../assets/img/cv4.png';
-import cv from '../../assets/img/cv.png';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css'; 
-import Roll from 'react-reveal/Roll';
-import Zoom from 'react-reveal/Zoom';
-import Flip from 'react-reveal/Flip';
+import React, { Component } from "react";
+import "../cv_img/style.css";
+import cv1 from "../../assets/img/cv1.png";
 
-const images = [
-  cv1,cv2,cv3,cv4
-];
+import cv from "../../assets/img/cv.png";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import Roll from "react-reveal/Roll";
+import Zoom from "react-reveal/Zoom";
+import Flip from "react-reveal/Flip";
+
+const images = [cv1];
 
 export default class CRI extends Component {
   constructor(props) {
@@ -27,35 +23,26 @@ export default class CRI extends Component {
   render() {
     const { photoIndex, isOpen } = this.state;
     return (
-     
       <Roll right>
-        <Zoom right> 
-        <Flip right>
-        <div>
-          <img src={cv} onClick={() => this.setState({ isOpen: true })} className="cv_img_mouse" alt="Ethan Sayagh CV preview"/>
-            {isOpen && (
+        <Zoom right>
+          <Flip right>
+            <div>
+              <img
+                src={cv}
+                onClick={() => this.setState({ isOpen: true })}
+                className="cv_img_mouse"
+                alt="Ethan Sayagh CV preview"
+              />
+              {isOpen && (
                 <Lightbox
                   mainSrc={images[photoIndex]}
-                  nextSrc={images[(photoIndex + 1) % images.length]}
-                  prevSrc={images[(photoIndex + images.length - 1) % images.length]}
                   onCloseRequest={() => this.setState({ isOpen: false })}
-                  onMovePrevRequest={() =>
-                    this.setState({
-                      photoIndex: (photoIndex + images.length - 1) % images.length,
-                    })
-                  }
-                  onMoveNextRequest={() =>
-                    this.setState({
-                      photoIndex: (photoIndex + 1) % images.length,
-                    })
-                  }
                 />
               )}
-        </div>
-        </Flip>
+            </div>
+          </Flip>
         </Zoom>
       </Roll>
-     
     );
   }
 }
