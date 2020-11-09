3d30caf2ba58f0aa881abcd6bf51a4dc64cd76b6
ethanolle <ethan.sayagh@gmail.com>
2020-11-09 14:07:38 +0200

    added logo download

--- src/components/cv_img/index.js
+++ src/components/cv_img/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "../cv_img/style.css";
 import cv1 from "../../assets/img/cv1.png";
-
 import cv from "../../assets/img/cv.png";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css";
 import Roll from "react-reveal/Roll";
 import Zoom from "react-reveal/Zoom";
 import Flip from "react-reveal/Flip";
+import { EmailShareButton } from "react-share";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import { EmailIcon } from "react-share";
 
 const images = [cv1];
 
@@ -23,26 +26,42 @@ export default class CRI extends Component {
   render() {
     const { photoIndex, isOpen } = this.state;
     return (
-      <Roll right>
-        <Zoom right>
-          <Flip right>
-            <div>
-              <img
-                src={cv}
-                onClick={() => this.setState({ isOpen: true })}
-                className="cv_img_mouse"
-                alt="Ethan Sayagh CV preview"
-              />
-              {isOpen && (
-                <Lightbox
-                  mainSrc={images[photoIndex]}
-                  onCloseRequest={() => this.setState({ isOpen: false })}
+      <div>
+        <Roll right>
+          <Zoom right>
+            <Flip right>
+              <div>
+                <img
+                  src={cv}
+                  onClick={() => this.setState({ isOpen: true })}
+                  className='cv_img_mouse'
+                  alt='Ethan Sayagh CV preview'
                 />
-              )}
-            </div>
-          </Flip>
-        </Zoom>
-      </Roll>
+                {isOpen && (
+                  <Lightbox
+                    toolbarButtons={[
+                      // <EmailShareButton
+                      //   url={window.location.href}
+                      //   children={<EmailIcon size={32} round={true} />}
+                      // />,
+                      <AiOutlineDownload
+                        href='/images/myw3schoolsimage.jpg'
+                        download='w3logo'
+                        className='test'
+                        size={25}
+                        // round={true}
+                        // onclick="window.open('file.doc')"
+                      ></AiOutlineDownload>,
+                    ]}
+                    mainSrc={images[photoIndex]}
+                    onCloseRequest={() => this.setState({ isOpen: false })}
+                  />
+                )}
+              </div>
+            </Flip>
+          </Zoom>
+        </Roll>
+      </div>
     );
   }
 }
