c71e7728a5c2776c2e44f06b9e80bc5448670105
ethanolle <ethan.sayagh@gmail.com>
2020-11-22 12:34:47 +0200

    updated phone

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,11 +4,15 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <Container className="footer" fluid={true}>
+    <Container className='footer' fluid={true}>
       <Row>
         <Col xl={12}>
-          Copyright © 2018-{new Date().getFullYear()}{" "}
-          <strong>Ethan Sayagh</strong>, <small>V2.3</small>
+          Copyright © 2018-{new Date().getFullYear()}
+          <strong>Ethan Sayagh</strong>,
+          <small>
+            V2.3 - Mail: <strong>ethan.sayagh@gmail.com</strong> - Phone:
+            <strong> +(972) 584888635 </strong>
+          </small>
         </Col>
       </Row>
     </Container>
